5c52cc679400ec9342a0533114515366e0c72d86
Attila <attila.gyorgy@szitar.hu>
2025-05-12 14:09:32 +0200

    init

---
+++ android/app/src/main/java/griddle/crunch/clientside/MainActivity.java
@@ -0,0 +1,5 @@
+package griddle.crunch.clientside;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'griddle.crunch.clientside',
+  appName: 'griddle-crunch-clientside',
+  webDir: 'dist'
+};
+
+export default config;
